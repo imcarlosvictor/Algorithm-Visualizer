91b85e5c5e10af66a9d222c42ee5c8621e724a16
Lucas <imcvlucas@gmail.com>
2025-11-18 03:30:55 -0500

    implemented tile class

--- include/tile.h
+++ include/tile.h
@@ -1,1 +1,45 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+
+// Determines the state of the tile every time it is reset
+enum TileState
+{
+    Start = 0,
+    End,
+    Path,
+    Explored,
+    Floor,
+    Wall
+};
+
+// Add weights to the tile for pathfinding algorithm
+
+class Tile
+{
+    public:
+        Tile(int length, int width, int x_coord, int y_coord);
+        void CreateTile();
+        void DrawTile(sf::RenderWindow& window);
+        void setStartPoint();
+        void setEndPoint();
+        void setPath();
+        void setExplored();
+        void setFloor();
+        void setWall();
+        void setCoordinate(int x, int y);
+        long unsigned int getXCoord();
+        long unsigned int getYCoord();
+        TileState getTileState();
+        int getTileLength();
+        int getTileWidth();
+
+    private:
+        sf::RectangleShape tile_;
+        TileState state_;
+        int length_;
+        int width_;
+        long unsigned int x_coord_;
+        long unsigned int y_coord_;
+};
---
+++ src/tile.cpp
@@ -0,0 +1,82 @@
+#include "../include/tile.h"
+
+Tile::Tile(int length, int width, int x_coord, int y_coord)
+{
+    this->tile_ = sf::RectangleShape(sf::Vector2f(this->length_, this->width_));
+    this->state_ = Floor;
+    this->length_ = length;
+    this->width_ = width;
+    this->x_coord_ = x_coord;
+    this->y_coord_ = y_coord;
+}
+
+void Tile::CreateTile()
+{
+    this->tile_.setSize(sf::Vector2f(this->length_, this->width_));
+    this->tile_.setFillColor(sf::Color(255,255,255));
+    this->tile_.setOutlineColor(sf::Color(64,64,64));
+    this->tile_.setOutlineThickness(.5);
+    this->tile_.setPosition(this->x_coord_, this->y_coord_);
+}
+
+void Tile::DrawTile(sf::RenderWindow& window)
+{
+    window.draw(this->tile_);
+}
+
+void Tile::setStartPoint()
+{
+    this->tile_.setFillColor(sf::Color(34,139,34));
+    this->state_ = Start;
+}
+
+void Tile::setEndPoint()
+{
+    this->tile_.setFillColor(sf::Color(244,48,10));
+    this->state_ = End;
+}
+
+void Tile::setPath()
+{
+    this->tile_.setFillColor(sf::Color(0,87,255));
+    this->state_ = Path;
+}
+
+void Tile::setExplored()
+{
+    this->tile_.setFillColor(sf::Color(220,226,254));
+    this->state_ = Explored;
+}
+
+void Tile::setFloor()
+{
+    this->tile_.setFillColor(sf::Color(255,255,255));
+    this->state_ = Floor;
+}
+
+void Tile::setWall()
+{
+    this->tile_.setFillColor(sf::Color(64,64,64));
+    this->state_ = Wall;
+}
+
+void Tile::setCoordinate(int x, int y)
+{
+    this->x_coord_ = x;
+    this->y_coord_ = y;
+}
+
+long unsigned int Tile::getXCoord()
+{
+    return x_coord_;
+}
+
+long unsigned int Tile::getYCoord()
+{
+    return y_coord_;
+}
+
+TileState Tile::getTileState()
+{
+    return this->state_;
+}
